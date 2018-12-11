a65b60d0ab65b2d84166c42df86dde71e9ebd50c
acichecki <a.cichecki@gmail.com>
2018-12-11 11:02:33 +0100

    HeaderToImageConverter

---
+++ SanityArchiver/SanityArchiver.DesktopUI/Converters/HeaderToImageConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using SanityArchiver.Application.Models.Data;
+
+namespace SanityArchiver.DesktopUI.Converters
+{
+
+        [ValueConversion(typeof(DirectoryItemType), typeof(BitmapImage))]
+        public class HeaderToImageConverter : IValueConverter
+        {
+            public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return new BitmapImage(new Uri($"pack://application:,,,/Images/{value}.png"));
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
