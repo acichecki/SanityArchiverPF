a1ca27ce1c27e7e02bde18ee018e049fa09c918f
acichecki <a.cichecki@gmail.com>
2018-12-11 11:23:28 +0100

    Added NugetPackage FodyWeavers and modified BaseViewModel

--- SanityArchiver/SanityArchiver.DesktopUI/ViewModels/BaseViewModel.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/ViewModels/BaseViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel;
+using PropertyChanged;
 
 namespace SanityArchiver.DesktopUI.ViewModels
 {
     /// <summary>
     /// A base View Model that fires Property Changed events as needed.
     /// </summary>
-    [ImplementPropertChanged]
+    [AddINotifyPropertyChangedInterface]
 
     public class BaseViewModel : INotifyPropertyChanged
     {
